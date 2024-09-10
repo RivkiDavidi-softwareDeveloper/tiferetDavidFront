dede9a0960831e770bc7c2dc00e2235d525e5c57
RivkiDavidi-softwareDeveloper <rivkid4862@gmail.com>
2024-09-10 13:16:49 +0300

    10.09

--- src/app/components/deshbord/deshbord.component.ts
+++ src/app/components/deshbord/deshbord.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { interval } from 'rxjs';
 import { map, take } from 'rxjs/operators';
+import { Worker } from '../../models/worker.class';
 @Component({
   selector: 'app-deshbord',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './deshbord.component.html',
   styleUrl: './deshbord.component.scss',
- 
+
 })
 export class DeshbordComponent {
+  listWorkers: Array<Worker> = []
+  codeFilter = -1
+  // -1 כולל
+  //-2 כולל בנים
+  //-3 כולל בנות
+  //אחר - קוד פעיל
+  amountWorkers = 30
+  amountChonchim = 11
+  amountStudentPail = 82
+  amountStudentAll = 321
+
+  matAmounts:Array<Array<number>>=[[1,2,3,4,888],[5,6,7,8,888],[9,10,11,12,888],[13,14,15,16,88]]
+
+//////ישן
   amountBWorkers = 0
   amountBStudents = 0
   amountBActivites = 0
@@ -23,88 +38,72 @@ export class DeshbordComponent {
   amountGKilometers = 0
 
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private cdRef: ChangeDetectorRef,) { }
 
 
-   ngOnInit(): void {
+  ngOnInit(): void {
+    this.getAllWorkers()
 
- /*      this.general() */
-/* 
-  //עובדים
-  interval(0.1)
-      .pipe(take(this.amountBWorkers))
-      .subscribe(val => {
-    this.amountBWorkers1 = val;
-  });
-//חניכים
-interval(0.1)
-  .pipe(take(this.amountBStudents))
-  .subscribe(val => {
-    this.amountBStudents1 = val;
-  });
-//עובדות
-interval(0.1)
-  .pipe(take(this.amountGWorkers))
-  .subscribe(val => {
-    this.amountGWorkers1 = val;
-  });
-//חניכות
-interval(0.1)
-  .pipe(take(this.amountGStudents))
-  .subscribe(val => {
-    this.amountGStudents1 = val;
-  });
-     interval(0.0000000000000000001) // Emit a value every second
-      .pipe(
-        take(45829 / 30), // (45829 / 3) = 15276.33, so emit values up to 15276
-        map(val => val * 30) // Increase each emitted value by 3
-      )
-      .subscribe(val => {
-        this.amountBWorkers = val; // Update the number every second
-      });  */
+    this.general()
+
+
   }
- general() {
-  //בנים
-  //עובדים
-/*   await new Promise<void>((resolve, reject) => { */
+  general() {
+    //בנים
+    //עובדים
+    /*   await new Promise<void>((resolve, reject) => { */
     this.api.getAmountWorkers("null", 0, 1, 0, 0).subscribe(Date => {
       this.amountBWorkers = Date
-     });  /*  resolve(); // מסמן שהפעולה הושלמה
+    });  /*  resolve(); // מסמן שהפעולה הושלמה
   }); */
 
-  //חניכים
-  this.api.getAmountStudents("null", 0, 1, 0, -1).subscribe(Date => {
-    this.amountBStudents = Date
-  })
-  //פעילויות
-  //קילומטרים+
-  var day = new Date();
-  this.api.AountsActivities("null", "null",0, 1, -1, -1, Number(day.getMonth) + 1,0, 0).subscribe(Date => {
-    this.amountBActivites = Date[0];
-    this.amountBKilometers = Date[3]
-  })
-  //בנות
-  //עובדות
-  this.api.getAmountWorkers("null", 0, 2, 0, 0).subscribe(Date => {
-    this.amountGWorkers = Date
-  })
-  //חניכות
-  this.api.getAmountStudents("null", 0, 2, 0, -1).subscribe(Date => {
-    this.amountGStudents = Date
-    console.log("חניכות  " + Date)
+    //חניכים
+    this.api.getAmountStudents("null", 0, 1, 0, -1).subscribe(Date => {
+      this.amountBStudents = Date
+    })
+    //פעילויות
+    //קילומטרים+
+    var day = new Date();
+    this.api.AountsActivities("null", "null", 0, 1, -1, -1, Number(day.getMonth) + 1, 0, 0).subscribe(Date => {
+      this.amountBActivites = Date[0];
+      this.amountBKilometers = Date[3]
+    })
+    //בנות
+    //עובדות
+    this.api.getAmountWorkers("null", 0, 2, 0, 0).subscribe(Date => {
+      this.amountGWorkers = Date
+    })
+    //חניכות
+    this.api.getAmountStudents("null", 0, 2, 0, -1).subscribe(Date => {
+      this.amountGStudents = Date
+      console.log("חניכות  " + Date)
 
-  })
-  //פעילויות
-  //קילומטרים
-  this.api.AountsActivities("null","null", 0, 2, -1, -1, Number(day.getMonth) + 1, 0,0).subscribe(Date => {
-    this.amountGActivites = Date[0];
-    this.amountGKilometers = Date[3]
-  })
-}
-//מוסיפה פסיקים
-addCommasToNumber(num: number): string {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+    })
+    //פעילויות
+    //קילומטרים
+    this.api.AountsActivities("null", "null", 0, 2, -1, -1, Number(day.getMonth) + 1, 0, 0).subscribe(Date => {
+      this.amountGActivites = Date[0];
+      this.amountGKilometers = Date[3]
+    })
+  }
+  //רשימת פעילים
+  getAllWorkers() {
+    this.api.getWorkers(0, 0, 0, 0).subscribe(Date => {
+      this.listWorkers = []
+      this.listWorkers.push(...Date)
+      this.cdRef.detectChanges();
+    }
+    )
+  }
+  //בחירת סינון
+  selectFilter(event: Event) {
+    var num: number = Number((event.target as HTMLInputElement).value)
+    this.codeFilter = num
+  }
+  //מוסיפה פסיקים
+  addCommasToNumber(num: number): string {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
 
 }
