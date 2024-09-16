80a215cb5b9d9fc930198d8a692b4d398b3f4d3b
RivkiDavidi-softwareDeveloper <rivkid4862@gmail.com>
2024-09-16 16:16:55 +0300

    16.09

--- src/app/components/deshbord/deshbord.component.ts
+++ src/app/components/deshbord/deshbord.component.ts
@@ -22,102 +22,65 @@ export class DeshbordComponent {
   //אחר - קוד פעיל
 
 
-  matAmounts: Array<Array<number>> = [[0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0],[0,0,0,0,0], [0,0,0,0,0],[0,0,0,0,0], [0,0,0,0,0]]
+  matAmounts: Array<Array<number>> = [[0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
   constructor(private api: ApiService, private cdRef: ChangeDetectorRef,) { }
 
 
   // חישוב stroke-dasharray לפי ערך של ahuz
-  getStrokeDasharray(ahuz:number): string {
+  getStrokeDasharray(ahuz: number): string {
     const percentage = Math.abs(ahuz);
     return `${percentage}, 100`;
   }
 
-generalDeshbord(){
-  this.api.DisplayDeshbord(this.codeFilter).subscribe(Date=>
-  {
-    this.matAmounts=[]
-    this.matAmounts.push(...Date);
+  generalDeshbord() {
+    this.api.DisplayDeshbord(this.codeFilter).subscribe(Date => {
+      this.matAmounts = []
+      this.matAmounts.push(...Date);
+    }
+    )
   }
-  )
-}
+  ngOnInit(): void {
+      this.getAllWorkers()
+    
+    this.generalDeshbord()
 
-
-//////ישן
-amountBWorkers = 0
-amountBStudents = 0
-amountBActivites = 0
-amountBKilometers = 0
-amountGWorkers = 0
-amountGStudents = 0
-amountGActivites = 0
-amountGKilometers = 0
-
-
-
-
-ngOnInit(): void {
-/*   this.getAllWorkers()
-    this.general() */
-this.generalDeshbord()
-
-}
-general() {
-  //בנים
-  //עובדים
-  /*   await new Promise<void>((resolve, reject) => { */
-  this.api.getAmountWorkers("null", 0, 1, 0, 0).subscribe(Date => {
-    this.amountBWorkers = Date
-  });  /*  resolve(); // מסמן שהפעולה הושלמה
-  }); */
-
-  //חניכים
-  this.api.getAmountStudents("null", 0, 1, 0, -1).subscribe(Date => {
-    this.amountBStudents = Date
-  })
-  //פעילויות
-  //קילומטרים+
-  var day = new Date();
-  this.api.AountsActivities("null", "null", 0, 1, -1, -1, Number(day.getMonth) + 1, 0, 0).subscribe(Date => {
-    this.amountBActivites = Date[0];
-    this.amountBKilometers = Date[3]
-  })
-  //בנות
-  //עובדות
-  this.api.getAmountWorkers("null", 0, 2, 0, 0).subscribe(Date => {
-    this.amountGWorkers = Date
-  })
-  //חניכות
-  this.api.getAmountStudents("null", 0, 2, 0, -1).subscribe(Date => {
-    this.amountGStudents = Date
-    console.log("חניכות  " + Date)
-
-  })
-  //פעילויות
-  //קילומטרים
-  this.api.AountsActivities("null", "null", 0, 2, -1, -1, Number(day.getMonth) + 1, 0, 0).subscribe(Date => {
-    this.amountGActivites = Date[0];
-    this.amountGKilometers = Date[3]
-  })
-}
-//רשימת פעילים
-getAllWorkers() {
-  this.api.getWorkers(0, 0, 0, 0).subscribe(Date => {
-    this.listWorkers = []
-    this.listWorkers.push(...Date)
-    this.cdRef.detectChanges();
   }
-  )
-}
-//בחירת סינון
-selectFilter(event: Event) {
-  var num: number = Number((event.target as HTMLInputElement).value)
-  this.codeFilter = num
-  this.generalDeshbord()
-}
-//מוסיפה פסיקים
-addCommasToNumber(num: number): string {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+
+  //////ישן
+
+
+
+
+
+
+
+  //רשימת פעילים
+  getAllWorkers() {
+    this.api.getWorkers(0, 0, 0, 0).subscribe(Date => {
+      this.listWorkers = []
+      this.listWorkers.push(...Date)
+      this.cdRef.detectChanges();
+    }
+    )
+  }
+  //בחירת סינון
+  selectFilter(event: Event) {
+    var num: number = Number((event.target as HTMLInputElement).value)
+    this.codeFilter = num
+    this.generalDeshbord()
+  }
+  //מוסיפה פסיקים
+  addCommasToNumber(num: number): string {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  //מחלצת שעות מזמן
+  hours(time: number): string {
+    return (Math.floor(time / 60)).toString();
+  }
+  //מחלצת דקות מזמן
+  minutes(time: number): string {
+    return (time % 60).toString();
+  }
 
 
 }
