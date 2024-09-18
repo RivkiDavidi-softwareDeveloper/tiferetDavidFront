4f130cc97c86a69bfd64142e8152a8cb5be85dfc
RivkiDavidi-softwareDeveloper <rivkid4862@gmail.com>
2024-09-18 18:08:38 +0300

    18.09 טבלאות חדשות

---
+++ src/app/models/sharer.class.ts
@@ -0,0 +1,24 @@
+import { Parentt } from "./parent.class";
+
+export class Sharer {
+    constructor(
+      public Sh_code: number,
+      public Sh_ID: string,
+      public Sh_gender: number,
+      public Sh_name: string,
+      public Sh_Fname: string,
+      public Sh_birthday: string,
+      public Sh_father_code: number,
+      public Sh_mother_code: number,
+      public Sh_city_code: number,
+      public Sh_address: string,
+      public Sh_cell_phone: string,
+      public Sh_phone: string,
+      public Sh_name_school_bein_hazmanim: string,
+      public Sh_nusah_tfila: string,
+      public Parent: Parentt | undefined,
+      public Parent1: Parentt | undefined
+    ){
+    }
+}
+
---
+++ src/app/models/sharerForProject.class.ts
@@ -0,0 +1,8 @@
+export class SharerForProject {
+    constructor(
+      public SFP_code: number,
+      public SFP_code_project: number,
+      public SFP_code_Sharer: number
+
+    ){}
+  }
