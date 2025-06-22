8ff5285d8e97cd32dd313b202ae2fbb30bea8cbb
RivkiDavidi-softwareDeveloper <rivkid4862@gmail.com>
2025-06-22 10:50:57 +0300

    22.06 server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 // שים את שם התיקייה בתוך dist במקום PROJECT_NAME
-app.use(express.static(path.join(__dirname, 'dist/PROJECT_NAME')));
+app.use(express.static(path.join(__dirname, 'dist/tiferet-david-app/browser')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/PROJECT_NAME/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/tiferet-david-app/browser/index.html'));
 });
 
 const port = process.env.PORT || 8080;
