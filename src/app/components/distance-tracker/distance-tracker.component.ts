6795dd66cb0af37dd6524fc2094b592c69ae9ab5
RivkiDavidi-softwareDeveloper <rivkid4862@gmail.com>
2025-06-22 13:04:40 +0300

    22.06 הוספת חישוב דרך בקילומטרים

--- src/app/components/distance-tracker/distance-tracker.component.ts
+++ src/app/components/distance-tracker/distance-tracker.component.ts
@@ -3,6 +3,7 @@ import { LocationService } from '../../services/location.service';
 import { CommonModule } from '@angular/common';
 import { interval, Subscription } from 'rxjs';
 import { GeocodingService } from '../../services/geocoding.service';
+import { DistanceService } from '../../services/distance.service';
 
 @Component({
   selector: 'app-distance-tracker',
@@ -15,7 +16,7 @@ import { GeocodingService } from '../../services/geocoding.service';
 
 export class DistanceTrackerComponent implements OnInit, OnDestroy {
 
-  distance: number = 0;
+/*   distance: number = 0;
   private previousPosition: GeolocationPosition | null = null;
   private trackingSubscription: Subscription | null = null;
   private intervalId: any;
@@ -25,120 +26,205 @@ export class DistanceTrackerComponent implements OnInit, OnDestroy {
   startLocation: { latitude: number, longitude: number, accuracy: number, address: string } | null = null;
   currentLocation: { latitude: number, longitude: number, accuracy: number, address: string } | null = null;
 
-  //טיימר זמן
-  @Output() start: EventEmitter<boolean> = new EventEmitter()
-  @Output() stop: EventEmitter<boolean> = new EventEmitter()
-  @Output() save: EventEmitter<Array<string>> = new EventEmitter()
-  @Input() ifDisplayStopAndSaveTime:boolean=false;
-startTimer() {
-  this.start.emit(true)
-}
-stopTimer() {
-  this.stop.emit(true)
-}
-saveTimer() {
-  if (this.startLocation && this.currentLocation)
-    this.save.emit([this.distance.toString(), this.startLocation?.address, this.currentLocation?.address])
-}
-constructor(private locationService: LocationService, private geocodingService: GeocodingService) { }
+ */
+/*   startTimer() {
+    this.start.emit(true)
+  }
+  stopTimer() {
+    this.stop.emit(true)
+  }
+  saveTimer() {
+    if (this.startLocation && this.currentLocation)
+      this.save.emit([this.distance.toString(), this.startLocation?.address, this.currentLocation?.address])
+  }    constructor(private locationService: LocationService, private geocodingService: GeocodingService, private distanceService: DistanceService) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void {
+  }
 
-ngOnDestroy(): void {
-  this.stopTracking();
-}
-
-startTracking(): void {
-  this.trackingSubscription = this.locationService.getPosition().subscribe(
-    (position) => {
-      if (!this.previousPosition) {
-        this.previousPosition = position;
-        this.startLocation = {
+  ngOnDestroy(): void {
+    this.stopTracking();
+  }
+  //התחלה
+  startTracking(): void {
+    this.trackingSubscription = this.locationService.getPosition().subscribe(
+      (position) => {
+        if (!this.previousPosition) {
+          this.previousPosition = position;
+          this.startLocation = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            accuracy: position.coords.accuracy,
+            address: ''
+          };
+          this.updateAddress(this.startLocation);
+        }
+        this.currentLocation = {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           accuracy: position.coords.accuracy,
           address: ''
         };
-        this.updateAddress(this.startLocation);
+        this.updateAddress(this.currentLocation);
+      },
+      (error) => console.error(error)
+    );
+
+    this.intervalId = setInterval(() => {
+      if (this.previousPosition) {
+        this.locationService.getPosition().subscribe((currentPosition) => {
+          if (this.previousPosition) {
+            const distanceIncrement = this.calculateDistance(
+              this.previousPosition.coords.latitude,
+              this.previousPosition.coords.longitude,
+              currentPosition.coords.latitude,
+              currentPosition.coords.longitude
+            );
+            this.distance += distanceIncrement;
+            this.previousPosition = currentPosition;
+            this.currentLocation = {
+              latitude: currentPosition.coords.latitude,
+              longitude: currentPosition.coords.longitude,
+              accuracy: currentPosition.coords.accuracy,
+              address: ''
+            };
+            this.updateAddress(this.currentLocation);
+          }
+        });
       }
-      this.currentLocation = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-        accuracy: position.coords.accuracy,
-        address: ''
-      };
-      this.updateAddress(this.currentLocation);
-    },
-    (error) => console.error(error)
-  );
-
-  this.intervalId = setInterval(() => {
-    if (this.previousPosition) {
-      this.locationService.getPosition().subscribe((currentPosition) => {
-        if (this.previousPosition) {
-          const distanceIncrement = this.calculateDistance(
-            this.previousPosition.coords.latitude,
-            this.previousPosition.coords.longitude,
-            currentPosition.coords.latitude,
-            currentPosition.coords.longitude
-          );
-          this.distance += distanceIncrement;
-          this.previousPosition = currentPosition;
-          this.currentLocation = {
-            latitude: currentPosition.coords.latitude,
-            longitude: currentPosition.coords.longitude,
-            accuracy: currentPosition.coords.accuracy,
-            address: ''
-          };
-          this.updateAddress(this.currentLocation);
-        }
-      });
-    }
-  }, 50000);
-}
-
-stopTracking(): void {
-  if(this.trackingSubscription) {
-  this.trackingSubscription.unsubscribe();
-}
-if (this.intervalId) {
-  clearInterval(this.intervalId);
-}
-this.saveTimer()
+    }, 50000);
   }
-//עדכון שם כתובת
-
-updateAddress(location: { latitude: number, longitude: number, accuracy: number, address: string }): void {
-  this.geocodingService.getAddress(location.latitude, location.longitude).subscribe(
-    (address) => {
-      location.address = address;
-      console.log(location.latitude)
-      console.log(location.longitude)
-      console.log(location.accuracy)
-      console.log(location.address)
-    },
-    (error) => {
-      location.address = 'כתובת לא זמינה';
-      console.error(error);
+  //סיום
+  stopTracking(): void {
+    if (this.trackingSubscription) {
+      this.trackingSubscription.unsubscribe();
     }
-  );
-}
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    this.saveTimer()
+  }
+  //עדכון שם כתובת
+  updateAddress(location: { latitude: number, longitude: number, accuracy: number, address: string }): void {
+    this.geocodingService.getAddress(location.latitude, location.longitude).subscribe(
+      (address) => {
+        location.address = address;
+        console.log(location.latitude)
+        console.log(location.longitude)
+        console.log(location.accuracy)
+        console.log(location.address)
+      },
+      (error) => {
+        location.address = 'כתובת לא זמינה';
+        console.error(error);
+      }
+    );
+  }
+  //חישוב מרחק
+  calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
+    const R = 6371;
+    const dLat = this.deg2rad(lat2 - lat1);
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+    return distance;
+  }
+ */
+  deg2rad(deg: number): number {
+    return deg * (Math.PI / 180);
+  }
 
-calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
-  const R = 6371;
-  const dLat = this.deg2rad(lat2 - lat1);
-  const dLon = this.deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c;
-  return distance;
-}
 
-deg2rad(deg: number): number {
-  return deg * (Math.PI / 180);
-}
+  /////שיטה 2
+    //טיימר זמן
+  @Output() start: EventEmitter<boolean> = new EventEmitter()
+  @Output() stop: EventEmitter<boolean> = new EventEmitter()
+  @Output() save: EventEmitter<Array<string>> = new EventEmitter()
+  @Input() ifDisplayStopAndSaveTime: boolean = false;
+  
+  originCoords: GeolocationCoordinates | null = null;
+  destinationCoords: GeolocationCoordinates | null = null;
+
+  distanceKm: number=0;
+  durationMin: number | null = null;
+  originAddress: string ="";
+  destinationAddress: string ="";
+
+    constructor( private distanceService: DistanceService) { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.setDestinationLocation();
+  }
+  //התחלה - מוצא
+  async setOriginLocation() {
+    await new Promise<void>((resolve, reject) => {
+
+      navigator.geolocation.getCurrentPosition(position => {
+        this.originCoords = position.coords;
+        resolve()
+
+      }, err => {
+        alert('שגיאה בזיהוי מיקום מוצא');
+        resolve()
+
+      });
+    });
+  }
+  //סיום - יעד
+  async setDestinationLocation() {
+    await new Promise<void>((resolve, reject) => {
+
+      navigator.geolocation.getCurrentPosition(position => {
+        this.destinationCoords = position.coords;
+        resolve()
+      }, err => {
+        alert('שגיאה בזיהוי מיקום יעד');
+        resolve()
+
+      });
+    });
+    this.calculateDistance2()
+  }
+  //חישוב
+  async calculateDistance2() {
+    await new Promise<void>((resolve, reject) => {
+
+      if (this.originCoords && this.destinationCoords) {
+
+        this.distanceService.getDistance(this.originCoords, this.destinationCoords)
+          .subscribe(data => {
+            this.distanceKm = data.distanceKm;
+            this.durationMin = data.durationMin;
+            this.originAddress = data.originAddress;
+            this.destinationAddress = data.destinationAddress;
+            resolve()
+
+          }, err => {
+            alert('שגיאה בקבלת נתונים מהשרת');
+            resolve()
+          });
+
+      } else {
+        alert('יש להגדיר מיקום מוצא ויעד');
+        resolve()
+
+      }
+    });
+  }
+    startTimer() {
+    this.start.emit(true)
+  }
+  stopTimer() {
+    this.stop.emit(true)
+  }
+  saveTimer() {
+    if (this.originCoords && this.destinationCoords)
+      this.save.emit([this.distanceKm.toString(), this.originAddress, this.destinationAddress])
+  }
 }
---
+++ src/app/services/distance.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DistanceService {
+
+  private apiUrl =  'http://localhost:3000/api';
+/*  private apiUrl = 'https://myserver-production-c24f.up.railway.app/api';
+ */
+  constructor(private http: HttpClient) {}
+
+  getDistance(origin: GeolocationCoordinates, destination: GeolocationCoordinates) {
+    const params = new HttpParams()
+      .set('originLat', origin.latitude.toString())
+      .set('originLng', origin.longitude.toString())
+      .set('destLat', destination.latitude.toString())
+      .set('destLng', destination.longitude.toString());
+
+    return this.http.get<any>(this.apiUrl, { params });
+  }
+}
