8caf88677654798fe5eb2b04e91940bbe9dfbdc2
RivkiDavidi-softwareDeveloper <rivkid4862@gmail.com>
2025-07-17 16:58:30 +0300

    17.07

--- src/app/services/distance.service.ts
+++ src/app/services/distance.service.ts
@@ -7,15 +7,12 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class DistanceService {
 
-  private apiUrl = 'http://localhost:3000/api/distance';
-/*  private apiUrl = 'https://myserver-production-c24f.up.railway.app/api/distance';
- */
+/*   private apiUrl = 'http://localhost:3000/api/distance';
+ */ private apiUrl = 'https://myserver-production-c24f.up.railway.app/api/distance';
+
   constructor(private http: HttpClient) {}
 
   getDistance(origin: GeolocationCoordinates, destination: GeolocationCoordinates) {
-            if (destination) {
-          console.log(`מיקום יעד: קו רוחב = ${destination.latitude}, קו אורך = ${destination.longitude}`);
-        }
     const params = new HttpParams()
       .set('originLat', origin.latitude.toString())
       .set('originLng', origin.longitude.toString())
